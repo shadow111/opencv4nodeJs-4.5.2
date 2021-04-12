e28b63b5bff2f39b1f0244a46f3c840c50dfe823
shadow111 <shadow111@users.noreply.github.com>
2021-04-12 23:56:57 +0300

    opencv build for nodejs

---
+++ test/externalMemTracking/other/index.test.js
@@ -0,0 +1,9 @@
+const { expect } = require('chai');
+const cv = require('../../requireCv')();
+const utils = require('../../utils')(cv);
+
+describe('External Memory Tracking', () => {
+  it.skip('no tests specified', () => {
+    // TODO ?
+  });
+});
